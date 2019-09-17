41cd188e903371a38146e77b0880321a9cb13052
Dan Flores <danf425@gmail.com>
2019-09-17 08:26:14 -0400

    plan update

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,19 +1,19 @@
 pkg_name=conjur
-pkg_origin=dmontanez
+pkg_origin=danf
 pkg_version="1.4.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_description="Conjur OSS"
+pkg_bin_dirs=(bin)
+pkg_svc_user=root
+pkg_svc_group=root
 pkg_upstream_url="https://github.com/cyberark/${pkg_name}"
 pkg_source="https://github.com/cyberark/${pkg_name}/archive/v${pkg_version}.tar.gz"
 pkg_shasum="6a78d299d48b6678db4caf311cc9490fedf50609eda64c4ed5eb5889ddbbe6e6"
 pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
 
-#pkg_svc_user=root
-#pkg_svc_group=root
 
 pkg_deps=(
-    core/git
     core/cacerts
     core/bundler
     core/docker 
@@ -21,6 +21,7 @@ pkg_deps=(
 )
 
 pkg_build_deps=(
+    core/git
 )
 
 pkg_bin_dirs=(bin)
@@ -30,19 +31,29 @@ do_unpack(){
  #   mkdir "${UNPACK_TO}"
  #   tar xzf "${HAB_CACHE_SRC_PATH}"/"v${pkg_version}".tar.gz --directory "${UNPACK_TO}"
     tar xzf "${HAB_CACHE_SRC_PATH}"/"${pkg_filename}" --directory ${HAB_CACHE_SRC_PATH}  
+    
 }
 
 do_build(){
-    ./build.sh
-    docker images | grep conjur
-    #return 0
+    return 0
 }
 
 do_install(){
- #   cd "${UNPACK_TO}"
- #   ./build.sh
-    cd dev
-    ./start
+    echo "here's cache path"
+    ls "${HAB_CACHE_SRC_PATH}"
+    echo "here's pkg_prefix"
+    ls "$pkg_prefix"
+    cp -r "${HAB_CACHE_SRC_PATH}"/"${pkg_name}"-"${pkg_version}" "$pkg_prefix"
+    cd "$pkg_prefix"/"${pkg_name}"-"${pkg_version}" 
+    ./build.sh
+    echo "helllo"   
+    echo "docker images | grep conjur"
+    docker images | grep conjur
+    ls
+ #   cd dev
+ #   ./start
+ 
+ 
  #   conjurctl role retrieve-key cucumber:user:admin
 }
 
